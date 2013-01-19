1f9fa092e1e5153e968f17570c89413866e05e80
Mark Cotton <tifosigoon@gmail.com>
2013-01-19 17:57:57 -0600

    saving images to local images folder

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ http.createServer(function(req, res) {
   if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
 	// parse a file upload
 	var form = new formidable.IncomingForm();
+	
+	form.uploadDir = 'images'
+	form.maxFieldsSize = 5 * 1024 * 1024;
 
 	form.parse(req, function(err, fields, files) {
 	  res.writeHead(200, {'content-type': 'text/plain'});
 	  res.write('received upload:\n\n');
 	  res.end(util.inspect({fields: fields, files: files}));
+	});
+	
+	form.on('progress', function(bytesReceived, bytesExpected) {
+	  var percent = (bytesReceived/bytesExpected * 100).toString()
+	  console.log(percent.substr(0, percent.indexOf('.')) + '% of ' + bytesExpected)
 	});
 
 	return;
