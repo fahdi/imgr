41f2b37a77078502ba4a68c46811ead23b3b5192
Mark Cotton <tifosigoon@gmail.com>
2013-02-10 16:46:51 -0600

    Added static file server and bootstrap to make it pretty

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ var formidable = require('formidable'),
 	http = require('http'),
 	util = require('util'),
 	knox = require('knox'),
-	router = require('router');
+	router = require('router'),
+	libpath = require('path'),
+	fs = require('fs'),
+    url = require("url"),
+    mime = require('mime');
 
 var route = router();
 
 var client = knox.createClient({ 
 	key: '', 
-	secret: '', 
-	bucket: '' 
-	})
+	secret: '',
+	bucket: ''
+});
 	
 	
 function pushToS3(files, orig_res) {
@@ -19,23 +23,16 @@ function pushToS3(files, orig_res) {
 		{ 'Content-Length': files.upload.size
   		, 'Content-Type': files.upload.type }, function(err, res){
 	  		
-	  if(err) console.log('Something bad happened putting file on S3')
-	  if(!err) console.log('File successfuly put file on S3')
-	  
-	  orig_res.writeHead(200, {'content-type': 'application/json'});
-	  
-	  var obj = {}
-	  obj.status_code = res.statusCode
-	  obj.file_name = file_name
-	  obj.location = 'https://s3.amazonaws.com/mcottondesign_images/' + file_name  
-	  orig_res.end(JSON.stringify(obj))
+	  if(err) console.log('Something bad happened putting file on S3');
+	  if(!err) console.log('File successfuly put file on S3');
+
 	});
 }
 
 function getFromS3(id, orig_res) {
 	client.getFile(id, function(err, res){
-	  if(err) console.log('Something bad happened getting file from S3' + res)
-	  if(!err) console.log('File successfuly fetched from S3 ' + res.statusCode)
+	  if(err) console.log('Something bad happened getting file from S3' + res);
+	  if(!err) console.log('File successfuly fetched from S3 ' + res.statusCode);
 	 	  
 	  orig_res.writeHead(res.statusCode, {'content-type': res.headers['content-type'] });
 	  
@@ -51,41 +48,36 @@ function getFromS3(id, orig_res) {
 }
 
 
-route.get('/', function(req, res) {
-	// show a file upload form
-	res.writeHead(200, {'content-type': 'text/html'});
-	res.end(
-	  '<form action="/upload" enctype="multipart/form-data" method="post">'+
-	  '<input type="file" accept="image/*" capture="camera" name="upload"><br>' +
-	  '<input type="submit" value="Upload">'+
-	  '</form>'
-	);
-})
-
 route.get('/image/{id}', function(req, res) {
 	var id = req.params.id;
-	console.log('DEBUG: matching /image/' + id)
+	console.log('DEBUG: matching /image/' + id);
 	
-	getFromS3(id, res)
-})
+	getFromS3(id, res);
+});
 
 route.post('/upload', function(req, res) {
 	// parse a file upload
 	var form = new formidable.IncomingForm();
 
-	form.uploadDir = 'images'
-	form.maxFieldsSize = 5 * 1024 * 1024;
+	form.uploadDir = 'images';
+	form.maxFieldsSize = 500 * 1024 * 1024;
 
 	form.parse(req, function(err, fields, files) {
-	  pushToS3(files, res)
+	  pushToS3(files, res);
 	});
 
 	form.on('progress', function(bytesReceived, bytesExpected) {
-	  var percent = (bytesReceived/bytesExpected * 100).toString()
-	  console.log(Math.ceil(percent) + '% of ' + bytesExpected)
+	  var percent = (bytesReceived/bytesExpected * 100).toString();
+	  console.log(Math.ceil(percent) + '% of ' + bytesExpected);
 	});
 
-})
+	form.on('end', function() {
+		res.end();
+	});
+
+
+
+});
 
 route.get('/list', function(req, res) {
 	client.list({ prefix: '' }, function(err, data){
@@ -94,6 +86,46 @@ route.get('/list', function(req, res) {
 		res.end(JSON.stringify(data.Contents));
 	});
 	
-})
+});
+
+
+route.get('/*', function(req, res) {
+	var uri = url.parse(req.url).pathname;
+    var filename = libpath.join('./public/', uri);
+
+	fs.exists(filename, function (exists) {
+        if (!exists) {
+            res.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            res.write("404 Not Found\n");
+            res.end();
+            return;
+        }
+ 
+        if (fs.statSync(filename).isDirectory()) {
+            filename += '/index.html';
+        }
+ 
+        fs.readFile(filename, "binary", function (err, file) {
+            if (err) {
+                response.writeHead(500, {
+                    "Content-Type": "text/plain"
+                });
+                res.write(err + "\n");
+                res.end();
+                return;
+            }
+ 
+            var type = mime.lookup(filename);
+            res.writeHead(200, {
+                "Content-Type": type
+            });
+            res.write(file, "binary");
+            res.end();
+        });
+    });
+
+});
 
 http.createServer(route).listen(3000);
